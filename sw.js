7def295862769caf77371ddd8874b80731afac12
Rafael Ramos <contactorafael05@gmail.com>
2025-09-07 21:55:22 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,32 @@
+const CACHE_NAME = 'app-cache-v' + new Date().getTime();
+
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open('app-cache').then(cache => {
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(['index.html','manifest.json']);
     })
   );
 });
 
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.map(k => { if (k !== CACHE_NAME) return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+    fetch(e.request)
+      .then(response => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      })
+      .catch(() => caches.match(e.request))
   );
 });
